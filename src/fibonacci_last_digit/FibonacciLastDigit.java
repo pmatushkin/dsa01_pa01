6d6633db6ec1547c099f853e7a47ecaab3f540c3
pmatushkin <pmatushkin@gmail.com>
2016-06-23 15:54:44 -0400

    feat: Pass Problem 2: The Last Digit of a Large Fibonacci Number

--- src/fibonacci_last_digit/FibonacciLastDigit.java
+++ src/fibonacci_last_digit/FibonacciLastDigit.java
@@ -4,9 +4,20 @@ import java.util.*;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigit(int n) {
-        return 0;
+        if (n <= 1)
+            return n;
+
+        int[] a = new int[n];
+        a[0] = 1;
+        a[1] = 1;
+
+        for (int i = 2; i < n; ++i) {
+            a[i] = (a[i - 1] + a[i - 2]) % 10;
+        }
+
+        return a[n - 1];
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
